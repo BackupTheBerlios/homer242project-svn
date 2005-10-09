6b3604724a993ee08424ab4b55262b1c3b07f892
homer242 <homer242@876f3860-a702-0410-9c58-f4e74bf5254e>
2005-10-09 07:00:58 +0000

    made a copy

---
+++ upsite/tags/upsite-alpha0.1/gui.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+#-*- coding: iso-8859-1 -*-
+
+## gui.py ( gui program )
+## Copyright © 2005 Viallard Anthony, homer242@gmail.com
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+# python
+import exceptions, os
+
+# wxpython
+import wx
+
+# me
+import utils
+from upsite import *
+
+## GLOBAL
+pwdProject = ""
+configFile = "_upsite" # pickle structure
+
+projectOpen = False
+projectModif = False
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+def showInfo(parent, message, titre) :
+	dg = wx.MessageDialog(parent, message, titre, style=wx.ICON_INFORMATION|wx.OK)
+	dg.ShowModal()
+	dg.Destroy()
+	
+def showError(parent, message, titre) :
+	dg = wx.MessageDialog(parent, message, titre, style=wx.ICON_ERROR|wx.OK)
+	dg.ShowModal()
+	dg.Destroy()
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################	
+class DialogConfig(wx.Dialog) :
+	def __init__(self, parent, title) :
+		wx.Dialog.__init__(self, parent, -1, title)
+		
+		# sizer
+		topSizer = wx.BoxSizer(wx.VERTICAL)
+		formSizer = wx.GridSizer(cols=2)
+		
+		# input
+		self.inputFtpHost = wx.TextCtrl(self, -1)
+		self.inputFtpPort = wx.TextCtrl(self, -1, "21")
+		self.inputFtpUser = wx.TextCtrl(self, -1)
+		self.inputFtpPass = wx.TextCtrl(self, -1, style=wx.TE_PASSWORD)
+		self.inputFtpPwd = wx.TextCtrl(self, -1)
+		
+		# sizer
+		formSizer.Add(wx.StaticText(self, -1, "Ip serveur ftp : "), 0, wx.ALL, 5)
+		formSizer.Add(self.inputFtpHost, 0, wx.ALL, 5)
+		formSizer.Add(wx.StaticText(self, -1, "Port serveur ftp : "), 0, wx.ALL, 5)
+		formSizer.Add(self.inputFtpPort, 0, wx.ALL, 5)
+		formSizer.Add(wx.StaticText(self, -1, "Login : "), 0, wx.ALL, 5)
+		formSizer.Add(self.inputFtpUser, 0, wx.ALL, 5)
+		formSizer.Add(wx.StaticText(self, -1, "Mot de passe : "), 0, wx.ALL, 5)
+		formSizer.Add(self.inputFtpPass, 0, wx.ALL, 5)
+		formSizer.Add(wx.StaticText(self, -1, "Repertoire dépot : "), 0, wx.ALL, 5)
+		formSizer.Add(self.inputFtpPwd, 0, wx.ALL, 5)
+		
+		topSizer.Add(formSizer, 0, wx.ALL, 5)
+		topSizer.Add(self.CreateButtonSizer(wx.CANCEL|wx.OK), 0, wx.ALIGN_CENTER|wx.ALL, 5)
+		
+		self.SetSizer(topSizer)
+		topSizer.Fit(self)
+	
+	def getFtpHost(self) :
+		return self.inputFtpHost.GetValue()
+		
+	def getFtpPort(self) :
+		return int(self.inputFtpPort.GetValue())
+		
+	def getFtpUser(self) :
+		return self.inputFtpUser.GetValue()
+		
+	def getFtpPass(self) :
+		return self.inputFtpPass.GetValue()
+		
+	def getFtpPwd(self) :
+		return self.inputFtpPwd.GetValue()
+		
+class MainFrame(wx.Frame) :
+	# id
+	IDMENU_NEWPROJ = wx.NewId()
+	IDMENU_OPENPROJ = wx.NewId()
+	IDMENU_SAVEPROJ = wx.NewId()
+	IDMENU_QUIT = wx.NewId()
+	IDMENU_ADDFILES = wx.NewId()
+	IDMENU_ADDDIR = wx.NewId()
+	IDMENU_UPLOAD = wx.NewId()
+	
+	IDTB_NEWPROJ = wx.NewId()
+	IDTB_OPENPROJ = wx.NewId()
+	IDTB_RECPROJ =wx.NewId()
+	IDTB_ADDFILESPROJ = wx.NewId()
+	IDTB_UPLOADPROJ = wx.NewId()
+	
+	def __init__(self, main, lastProject = None) :
+		wx.Frame.__init__(self, None, -1, str(main.appname) + " v" + str(main.appversion), 
+			size=(230, 320))
+
+		self.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_BTNFACE));
+		
+		# sizer
+		topSizer = wx.BoxSizer(wx.HORIZONTAL)
+		
+		# menu
+		self.menubar = wx.MenuBar()
+		
+		filemenu = wx.Menu()
+		filemenu.Append(self.IDMENU_NEWPROJ, "Nouveau projet")
+		filemenu.Append(self.IDMENU_OPENPROJ, "Ouvrir projet")
+		filemenu.Append(self.IDMENU_SAVEPROJ, "Sauvegarder projet")
+		filemenu.Append(self.IDMENU_QUIT, "Quitter")
+		
+		projmenu = wx.Menu()
+		#~ projmenu.Append(self.IDMENU_ADDDIR, "Ajouter un dossier")
+		#~ projmenu.Append(self.IDMENU_ADDFILES, "Ajouter fichiers")
+		projmenu.Append(self.IDMENU_UPLOAD, "Uploader")
+		
+		self.menubar.Append(filemenu, "Fichier")
+		self.menubar.Append(projmenu, "Projet")
+		
+		self.SetMenuBar(self.menubar)
+		
+		# toolbar
+		self.toobar = self.CreateToolBar()
+		
+		tsize = (16,16)
+		new_bmp =  wx.ArtProvider.GetBitmap(wx.ART_NEW, wx.ART_TOOLBAR, tsize)
+		open_bmp = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR, tsize)
+		rec_bmp = wx.ArtProvider.GetBitmap(wx.ART_FILE_SAVE, wx.ART_TOOLBAR, tsize)
+		up_bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_DIR_UP, wx.ART_TOOLBAR, tsize)
+		add_bmp = wx.ArtProvider.GetBitmap(wx.ART_ADD_BOOKMARK, wx.ART_TOOLBAR, tsize)
+		
+		self.toobar.AddSimpleTool(self.IDTB_NEWPROJ, new_bmp, "Nouveau projet")
+		self.toobar.AddSimpleTool(self.IDTB_OPENPROJ, open_bmp, "Ouvrir projet")
+		self.toobar.AddSimpleTool(self.IDTB_RECPROJ, rec_bmp, "Enregistrer projet")
+		self.toobar.AddSeparator()
+		self.toobar.AddSimpleTool(self.IDTB_ADDFILESPROJ, add_bmp, "Ajouter fichiers au projet")
+		self.toobar.AddSimpleTool(self.IDTB_UPLOADPROJ, up_bmp, "Uploader projet")
+		
+		self.toobar.Realize()
+		
+		# tree
+		self.tree = wx.TreeCtrl(self, -1)
+		self.tree.AddRoot("projet")
+		
+		# chargement arbre
+		if lastProject :
+			self.project = lastProject
+			try :
+				self.project.openProject()
+				self.populateWxTree() # on charge le projet
+				projectOpen = True
+			except IOError, detail :
+				showError(self, "Impossible d'ouvrir le fichier projet !\nDetail : %s" % detail, "Erreur !")
+			except :
+				showError(self, "Impossible de charger le fichier de configuration du projet !", "Erreur !")
+				
+		# event
+		self.Bind(wx.EVT_MENU, self.OnQuit, id=self.IDMENU_QUIT)
+		self.Bind(wx.EVT_TOOL, self.OnNewProject, id=self.IDTB_NEWPROJ)
+		self.Bind(wx.EVT_MENU, self.OnNewProject, id=self.IDMENU_NEWPROJ)
+		self.Bind(wx.EVT_TOOL, self.OnOpenProject, id=self.IDTB_OPENPROJ)
+		self.Bind(wx.EVT_MENU, self.OnOpenProject, id=self.IDMENU_OPENPROJ)
+		self.Bind(wx.EVT_TOOL, self.OnRecProject, id=self.IDTB_RECPROJ)
+		self.Bind(wx.EVT_MENU, self.OnRecProject, id=self.IDMENU_SAVEPROJ)
+		self.Bind(wx.EVT_MENU, self.OnAddDir, id=self.IDMENU_ADDDIR)
+		self.Bind(wx.EVT_TOOL, self.OnAddFiles, id=self.IDTB_ADDFILESPROJ)
+		self.Bind(wx.EVT_MENU, self.OnAddFiles, id=self.IDMENU_ADDFILES)
+		self.Bind(wx.EVT_TOOL, self.OnUpProject, id=self.IDTB_UPLOADPROJ)
+		self.Bind(wx.EVT_MENU, self.OnUpProject, id=self.IDMENU_UPLOAD)
+		
+		self.tree.Bind(wx.EVT_COMMAND_RIGHT_CLICK, self.OnTreeRightClick) # for msw
+		self.tree.Bind(wx.EVT_RIGHT_UP, self.OnTreeRightClick) # for gtk
+		
+		# sizer
+		topSizer.Add(self.tree, 1, wx.LEFT|wx.RIGHT|wx.BOTTOM|wx.EXPAND, 5)
+		
+		self.SetSizer(topSizer)
+
+	def OnQuit(self, event) :
+		self.Destroy()
+	
+	def OnNewProject(self, event) :
+		# où se trouve le projet ?
+		dlg = wx.DirDialog(self, "Choisissez le repertoire où se trouve le projet : ", style=wx.DD_DEFAULT_STYLE)
+		if dlg.ShowModal() == wx.ID_OK :
+			projectPath = Dir(dlg.GetPath())
+			dlg.Destroy()
+		else :
+			dlg.Destroy()
+			return
+			
+		# la configuration ftp
+		dlg = DialogConfig(self, "Configuration FTP")
+		if dlg.ShowModal() == wx.ID_OK :
+			ftpHost = dlg.getFtpHost()
+			ftpPort = dlg.getFtpPort()
+			ftpUser = dlg.getFtpUser()
+			ftpPass = dlg.getFtpPass()
+			ftpPwd = dlg.getFtpPwd()
+			dlg.Destroy()
+		else :
+			dlg.Destroy()
+			return
+		
+		# où enregistrer le fichier de configuration ?
+		dlg = wx.FileDialog(self, message="Sauvegarder le fichier de configuration du projet ... ",
+			defaultFile="_upsite", style=wx.SAVE)
+		
+		if dlg.ShowModal() == wx.ID_OK :
+			configFileProject = dlg.GetPath()
+			
+			# on ouvre le fichier (try ?)
+			f = open(configFileProject, 'w')
+			f.write("\n") # on écrit un truc
+			f.close()
+			
+			# on sauvegarde dans le fichier xml l'endroit où se trouve ce fichier de configuration
+			conf = ReadConfig("configuration.xml")
+			conf.writeXml(configFileProject)
+			
+			# 
+			self.project = Project(configFileProject)
+			self.project.createProject(projectPath)
+			self.project.ftphost = ftpHost
+			self.project.ftpport = ftpPort
+			self.project.ftpuser = ftpUser
+			self.project.ftppass = ftpPass
+			self.project.ftppwd = ftpPwd
+			
+			self.project.addDir(projectPath.pwd)
+			
+			self.populateWxTree()
+		
+		dlg.Destroy()
+		
+	def OnOpenProject(self, event) :
+		# où est le fichier ?
+		dlg = wx.FileDialog(self, message="Ouvrir le projet ... ",
+			defaultFile="_upsite", style=wx.OPEN|wx.CHANGE_DIR)
+		
+		if dlg.ShowModal() == wx.ID_OK :
+			path = dlg.GetPath()
+			
+			self.project = Project(path)
+			self.project.openProject()
+			
+			self.populateWxTree()
+		
+		dlg.Destroy()
+	
+	def OnRecProject(self, event) :
+		self.project.saveProject()
+		
+	def OnAddDir(self, event) :
+		pass
+		
+	def OnAddFiles(self, event) :
+		pass
+		
+	def OnUpProject(self, event) :
+		self.project.upload()
+		
+	def OnTreeRightClick(self, event) :
+		pass
+		#~ menu = wx.Menu()
+	
+		#~ self.PopupMenu(menu, wx.DefaultPosition)
+		#~ menu.Destroy()
+	
+	def OnPopupAdd(self, event) :
+		pass
+		
+	def OnPopupDel(self, event) :
+		pass
+		
+	def LoadFilesTree(self) :
+		# chargement du fichier de configuration du site
+		#~ if True : #try :
+		self.project.openProject()
+		#~ except Exception, detail :
+			#~ # erreur ... oui
+			#~ showError(self, "Erreur : Impossible de lire le fichier xml '%s', une erreur a été trouvé ! \nDetail : %s" % (self.project.projectFile, detail), "Erreur !")
+			#~ return -1
+			
+		self.tree.Hide()
+		
+		# on fait le ménage
+		self.tree.DeleteAllItems()
+		
+		# on explore et on ajoute
+		self.__sub_loadFilesTree(self.project.fileTree.rootItem, self.tree.GetRootItem())
+			
+		self.tree.Expand(self.tree.GetRootItem())
+		
+		self.tree.Show()
+		
+	def __sub_loadFilesTree(self, treeNode, pwdParentNode) :
+		child = self.tree.AppendItem(treeNode.GetName(), utils.getEndPwd(treeNode.GetName()))
+		
+		files = []
+		for fileintree in treeNode.GetChilds :
+			if fileintree.IsLeaf() :
+				files.append(fileintree.GetName())
+			else :
+				self.__sub_loadFilesTree(fileintree, child)
+				
+		for file in files :
+			self.tree.AppendItem(child, file)
+			
+		#~ for nom in os.listdir(pwdStr):
+			#~ chemin = os.path.join(pwdStr,nom)
+			#~ if os.path.isfile(chemin):
+				#~ if nom != configFile :
+					#~ files.append(nom)
+			#~ elif os.path.isdir(chemin):
+				#~ self.__sub_loadFilesTree(chemin, child)
+		
+	def populateWxTree(self) :
+		self.tree.Hide()
+		
+		# on fait le ménage
+		self.tree.DeleteAllItems()
+		
+		# on parcours l'arbre (objet python)
+		rootItem = self.project.fileTree.rootItem
+		
+		self.__sub_populateWxTree(self.tree.GetRootItem(), rootItem)
+		
+		# expand
+		self.tree.Expand(self.tree.GetRootItem())
+		
+		self.tree.Show()
+		
+	def __sub_populateWxTree(self, wxTreeNode, treeNode) :
+		# on a affaire à un repertoire
+		child = self.tree.AppendItem(wxTreeNode, getEndPwd(treeNode.Get().pwd))
+		
+		files = []
+		for fileintree in treeNode.GetChilds() :
+			if fileintree.IsLeaf() :
+				files.append(fileintree.Get().name)
+			else :
+				self.__sub_populateWxTree(child, fileintree)
+				
+		for file in files :
+			self.tree.AppendItem(child, file)
+			
---
+++ upsite/tags/upsite-alpha0.1/upsite.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+#-*- coding: iso-8859-1 -*-
+
+## upsite.py ( main program )
+## Copyright © 2005 Viallard Anthony, homer242@gmail.com
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+# python
+import sys, os
+import pickle
+from ftplib import FTP
+
+# wxpython
+import wx
+
+# xml
+from xml.dom.minidom import parse
+
+# exception
+import exceptions
+
+#
+import gui
+from tree import *
+from utils import *
+
+__appname__ = "upSite"
+__appversion__ = "0.1"
+
+##############################################################################
+
+class File :
+	def __init__(self, name, modtimestamp) :
+		self.name = name
+		self.modtimestamp = modtimestamp
+
+class Dir :
+	def __init__(self, pwd) :
+		self.pwd = pwd
+		
+class Project :
+	def __init__(self, projectFile) :
+		self.projectFile = projectFile
+		self.fileTree = None
+		
+		self.ftphost = ""
+		self.ftpport = 21
+		self.ftpuser = ""
+		self.ftppass = ""
+		self.ftppwd = ""
+		
+	def openProject(self) :
+		# on ouvre le fichier
+		f = open(self.projectFile, 'rb') # b=> binaire
+		
+		project = pickle.load(f) # vive pickle !
+		
+		self.fileTree = project.fileTree
+		self.ftphost = project.ftphost
+		self.ftpport = project.ftpport
+		self.ftpuser = project.ftpuser
+		self.ftppass = project.ftppass
+		self.ftppwd = project.ftppwd
+	
+		f.close()
+		
+	def saveProject(self) :
+		f = open(self.projectFile, 'w+b')
+		
+		pickle.dump(self, f)
+		
+		f.close()
+		
+	def createProject(self, dirItem) :
+		self.fileTree = Tree(dirItem)
+		
+	def addFiles(self, files) :
+		pass
+		
+	def addDir(self, pwd) :
+		# pour l'instant, on enregistre direct dans la racine
+		# aller hop, on parcours le dossier
+		self.__sub_addDir(self.fileTree.rootItem, pwd)
+		
+	def __sub_addDir(self, treeNode, pwd) :
+		for nom in os.listdir(pwd):
+			chemin = os.path.join(pwd,nom)
+			if os.path.isfile(chemin):
+				treeNode.Add( Node( File(nom, 0) ) )
+			elif os.path.isdir(chemin):
+				nodeDir = Node(Dir(chemin))
+				treeNode.Add(nodeDir)
+				self.__sub_addDir(nodeDir, chemin)
+		
+	def upload(self) :
+		"""
+		Fonction d'upload. Compare les dates de modifications
+		"""
+		# connexion au serveur ftp
+		self.ftpd = FTP()
+		self.ftpd.connect(self.ftphost, self.ftpport)
+		self.ftpd.login(self.ftpuser, self.ftppass)
+		
+		self.__openFtpDir(self.ftppwd)
+		# let's rock !
+		self.__sub_upload(self.fileTree.rootItem)
+		
+		self.ftpd.quit()
+		
+		# on sauvegarde les modifs
+		self.saveProject()
+		
+	def __sub_upload(self, treeNode) :
+		# on parcours l'arbre
+		for node in treeNode.GetChilds() :
+			if node.IsLeaf():
+				# on compare la date
+				file = node.Get()
+				fileName = treeNode.Get().pwd + os.sep + file.name
+				lastModts = os.stat(fileName)[7]
+				if lastModts > file.modtimestamp :
+					print "Upload -> " + fileName
+					# upload ftp !
+					f = open(fileName, 'rb') # on ouvre le fichier pour le transfert
+					# on change de repertoire sur le ftp
+					self.__openFtpDir(self.ftppwd+getRestPwd(self.fileTree.rootItem.Get().pwd, treeNode.Get().pwd))
+					self.ftpd.storbinary("STOR "+file.name, f)
+					file.modtimestamp = lastModts
+				continue
+			else :
+				# repertoire
+				self.__sub_upload(node)
+	
+	def __createFtpDir(self, pwd) :
+		self.ftpd.cwd("/")
+		ftppwd = pwd.lstrip("/")
+		for folder in ftppwd.split('/') :
+			try :
+				self.ftpd.cwd(folder)
+			except :
+				self.ftpd.mkd(folder)
+				self.ftpd.cwd(folder)
+				
+	def __openFtpDir(self, pwd) :
+		pwd = pwd.replace("\\", "/") # encore windows qui fait chier !
+		#~ print pwd
+		self.__createFtpDir(pwd)
+		
+##############################################################################
+class ReadConfig :
+	lastproject = ""
+	
+	def __init__(self, filename) :
+		self.filename = filename
+		
+	def readXml(self) :
+		self.doc = parse(self.filename)
+		ElemDB = self.doc.documentElement.getElementsByTagName("history")[0]
+		
+		self.lastproject = ElemDB.getAttribute("lastproject")
+		
+	def writeXml(self, lastProject="") :
+		f = file(self.filename, "w")
+		
+		f.write("<configuration>\n")
+		f.write("\t<history lastproject=\""+str(lastProject)+"\" />\n")
+		f.write("</configuration>\n")
+			
+		f.close()
+		
+		
+if __name__ == "__main__" :
+
+	class GApp(wx.App) :
+		def OnInit(self) :
+		## global variables
+			global __appname__, __appversion__
+			self.appname = __appname__
+			self.appversion = __appversion__
+		
+			## lecture du fichier de configuration
+			## par defaut :
+			##      host = localhost
+			##      port = 5432
+			##      database = lanmanager
+			##      user = 
+			##      password = 
+			conf = ReadConfig("configuration.xml")
+			try :
+				conf.readXml()
+			except exceptions.Exception, detail:
+				dg = wx.MessageDialog(None, "Impossible de charger la configuration (une erreur se trouve dans le fichier configuration.xml !)\nErreur : %s \n\nVoulez-vous remettre à defaut le fichier de configuration ?" % detail, "Erreur", style=wx.ICON_ERROR|wx.YES_NO)
+				if dg.ShowModal() == wx.ID_YES :
+					conf.writeXml()
+				else :
+					dg.Destroy()
+					sys.exit(1)
+				dg.Destroy()
+	
+			## si tout vas bien, on lance l'application graphique
+			if conf.lastproject :
+				project = Project(conf.lastproject)
+				frame = gui.MainFrame(self, project)
+			else :
+				frame = gui.MainFrame(self)
+			frame.Show(True)
+			self.SetTopWindow(frame)
+			return True
+
+	## on lance l'application
+	app = GApp(False)
+	## mainLoop
+	app.MainLoop()
